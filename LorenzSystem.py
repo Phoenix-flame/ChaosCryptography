2e91cc5660e9394eb81a8586059c44ef281ce42e
Phoenix_flame <alireza17010@gmail.com>
2021-02-03 14:59:37 +0330

    check params based on lyapExponents

--- LorenzSystem.py
+++ LorenzSystem.py
@@ -7,13 +7,12 @@ Created on Fri Jan 29 17:47:07 2021
 """
 
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from mpl_toolkits.mplot3d import Axes3D
 import ctypes
-import time
+
 
 class System:
     def __init__(self):
@@ -23,7 +22,6 @@ class System:
         self.sigma = 10.0
         self.beta = 8.0 / 3.0
         self.states = None
-        
 
     def model(self, state, t):
         x, y, z = state
@@ -40,27 +38,58 @@ class System:
         plt.draw()
         plt.show()
 
+    def get_lya_exp(self):
+        epsilon = 0.00001
+        lya_exp = []
+
+        for i in range(6):    
+            model1 = System()
+            for j in range(3):
+                model1.initial_state[j] = self.initial_state[j] if i != j else (self.initial_state[j] + epsilon) 
+            model1.beta = self.beta if i != 3 else (self.beta+epsilon)
+            model1.rho = self.rho if i != 4 else (self.rho+epsilon)
+            model1.sigma = self.sigma if i != 5 else (self.sigma+epsilon)
+            response = self.solve()
+            response1 = model1.solve()
+            abs_delta_X = abs(response[-1]-response1[-1])
+            lambda_matrix = 1/40*(np.log(abs_delta_X/epsilon))
+            lya_exp.append(np.amax(lambda_matrix))
+
+        return lya_exp
+
+
 class PRNG:
     def __init__(self):
         self.model = System()
-    
+
     def SetKey(self, initialValue, beta, rho, sigma):
         self.model.beta = beta
         self.model.rho = rho
         self.model.sigma = sigma
         self.model.initial_state = initialValue
-        
+
+    def check_params(self):
+        lya_exp = self.model.get_lya_exp()
+        for i in lya_exp:
+            if(i <= 0):
+                msg = 'INVALID PARAMS'
+                print(msg)
+                return
+        print('OK')
+
     def generate(self):
         response = self.model.solve()
+        self.check_params()                   ##########################################3
         response = response[range(50, len(response), 62)]
         code = ''
         for data in response:
             num = 0.0
             for vec in data:
                 num += vec
-            binaryForm = bin(ctypes.c_uint.from_buffer(ctypes.c_float(num)).value)[2:]
-            bit1 = binaryForm[-5]
-            bit2 = binaryForm[-4]
+            binaryForm = bin(ctypes.c_uint.from_buffer(
+                ctypes.c_float(num)).value)[2:]
+            bit1 = binaryForm[-5]  # 11
+            bit2 = binaryForm[-4]  # 17
             bit3 = binaryForm[-1]
             bit4 = binaryForm[-2]
             code += (bit1)
